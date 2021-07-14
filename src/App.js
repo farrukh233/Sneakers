3efe8a07208245129585d2b5691a4cde028d00a3
farrukh <farrukhx50@gmail.com>
2021-07-14 23:36:49 +0500

    Добавил поиск и его филтрацию

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
-
+  const [searchValue, setSearchValue] = React.useState("");
+  {
+    /*данные с бэкенда (mockApi*/
+  }
   React.useEffect(() => {
     fetch("https://60e358636c365a0017839276.mockapi.io/items")
       .then(res => {
@@ -22,6 +25,10 @@ function App() {
     setCartItems(prev => [...prev, obj]);
   };
 
+  const onChangeSearchInput = event => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className='wrapper clear'>
       {cartOpened && (
@@ -30,23 +37,43 @@ function App() {
       <Header onClickCart={() => setCartOpened(true)} />
       <div className='content p-40'>
         <div className='d-flex align-center justify-between mb-40'>
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу: "${searchValue}"`
+              : "Все кроссовки"}
+          </h1>
           <div className='search-block d-flex'>
             <img src='/img/search.svg' alt='Search' />
-            <input placeholder='Поиск...' />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className='clear cu-p'
+                src='/img/btn-remove.svg'
+                alt='Clear'
+              />
+            )}
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              placeholder='Поиск...'
+            />
           </div>
         </div>
 
         <div className='d-flex flex-wrap'>
-          {items.map(item => (
-            <Card
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onFavorite={() => console.log("Добавили в закладки")}
-              onPlus={obj => onAddToCart(obj)}
-            />
-          ))}
+          {/*фильтрация поиска*/}
+          {items
+            .filter(item => item.title.toLowerCase().includes(searchValue))
+            .map(item => (
+              <Card
+                key={item.title}
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onFavorite={() => console.log("Добавили в закладки")}
+                onPlus={obj => onAddToCart(obj)}
+              />
+            ))}
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header(props) {
   return (
     <header className='d-flex justify-between align-center p-40'>
       <div className='d-flex align-center'>
-        <img width={40} heigth={40} src='/img/logo.png' />
+        <img width={40} heigth={40} src='/img/logo.png' alt='logo' />
         <div>
           <h3 className='text-uppercase'> React Sneakers</h3>
           <p className='opacity-5'> Магазин лучших кросовок</p>
@@ -10,11 +10,11 @@ function Header(props) {
       </div>
       <ul className='d-flex'>
         <li onClick={props.onClickCart} className='mr-30 cu-p'>
-          <img width={18} heigth={18} src='/img/cart.png' />
+          <img width={18} heigth={18} src='/img/cart.png' alt='cart' />
           <span>1205 руб.</span>
         </li>
         <li>
-          <img width={18} heigth={18} src='/img/user.png' />
+          <img width={18} heigth={18} src='/img/user.png' alt='user' />
         </li>
       </ul>
     </header>
